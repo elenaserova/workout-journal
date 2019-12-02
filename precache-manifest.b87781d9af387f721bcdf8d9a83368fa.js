f2d79b5e951c4b2c3f296d1712f00f79ba7925a9
elenaserova <elein.serova@gmail.com>
2019-12-02 14:21:17 -0500

    Updates

---
+++ precache-manifest.b87781d9af387f721bcdf8d9a83368fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44188150e27f4ed93b163ed9891bd220",
+    "url": "/workout-journal/index.html"
+  },
+  {
+    "revision": "dd992649d017eae761f2",
+    "url": "/workout-journal/static/css/main.f66fc47a.chunk.css"
+  },
+  {
+    "revision": "ea2f21e04f4fddcd1805",
+    "url": "/workout-journal/static/js/2.39504b4a.chunk.js"
+  },
+  {
+    "revision": "dd992649d017eae761f2",
+    "url": "/workout-journal/static/js/main.2a8d6f2c.chunk.js"
+  },
+  {
+    "revision": "b1d26fc63312ee1f18a9",
+    "url": "/workout-journal/static/js/runtime-main.c3d8b7d0.js"
+  }
+]);
